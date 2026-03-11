73b698da93a6e5f8a14b4b859cdad40c90b7d0d2
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-03-11 08:54:56 +0100

    fix(ecs): add missing relationship files

---
+++ src/engine/ecs/Relation.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <utility>
+#include "engine/ecs/type.hpp"
+#include "engine/datastructure/EcsVec.hpp"
+
+namespace ecs {
+    class World;
+
+    struct RelatedIteratorNonRecursive {
+        Entity parent;
+        Entity *current;
+
+        std::pair<Entity, Entity> operator*() const;
+        RelatedIteratorNonRecursive &operator++();
+        bool operator!=(const RelatedIteratorNonRecursive &other) const;
+    };
+
+    struct RelatedIteratorRecursive {
+        World *world;
+        ComponentID source_id;
+
+        struct Level {
+            Entity parent;
+            Entity *current;
+            Entity *end;
+        };
+
+        Level stack[64];
+        uint32_t stack_size = 0;
+
+        RelatedIteratorRecursive(World *w, Entity start, ComponentID source_id);
+        RelatedIteratorRecursive();
+
+        std::pair<Entity, Entity> operator*() const;
+        RelatedIteratorRecursive &operator++();
+        bool operator!=(const RelatedIteratorRecursive &other) const;
+    };
+
+    struct RelatedRangeNonRecursive {
+        World *world;
+        Entity target;
+        ComponentID source_id;
+
+        RelatedIteratorNonRecursive begin() const;
+        RelatedIteratorNonRecursive end() const;
+    };
+
+    struct RelatedRangeRecursive {
+        World *world;
+        Entity target;
+        ComponentID source_id;
+
+        RelatedIteratorRecursive begin() const;
+        RelatedIteratorRecursive end() const;
+    };
+}
