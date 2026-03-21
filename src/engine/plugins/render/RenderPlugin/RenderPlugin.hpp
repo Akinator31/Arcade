ae7fc4735e8bb229e9baff762a09d0378ca90e80
Akinator31 <paveldewavrechin@gmail.com>
2026-03-21 18:27:01 +0100

    refactor(sprite): centered sprites in RenderSpriteSys

--- src/engine/plugins/render/RenderPlugin/RenderPlugin.hpp
+++ src/engine/plugins/render/RenderPlugin/RenderPlugin.hpp
@@ -4,14 +4,14 @@
 #include "engine/plugins/render/CameraPlugin/CameraPlugin.hpp"
 #include "engine/plugins/render/PositionPropagationPlugin/PositionPropagationPlugin.hpp"
 
-template<class Fn>
-Fn get_virtual(void *obj, const size_t index) {
-    void **vtable = *static_cast<void ***>(obj);
+template <class Fn>
+Fn get_virtual(void* obj, const size_t index) {
+    void** vtable = *static_cast<void***>(obj);
     return reinterpret_cast<Fn>(vtable[index]);
 }
 
 namespace render_plugin_impl {
-    inline GlobalPosition worldToCamera(const GlobalPosition &world_position, const GlobalPosition &camera_position) {
+    inline GlobalPosition worldToCamera(const GlobalPosition& world_position, const GlobalPosition& camera_position) {
         return {
             world_position.x - camera_position.x,
             world_position.y - camera_position.y
@@ -23,10 +23,10 @@ SYSTEM(RenderRectSys, With<GlobalPosition, Size, Color>, On<Render>) {
     ITER(view) {
         const ecs::Entity main_camera = camera_plugin_impl::mainCamera(view.world);
         const GlobalPosition main_camera_position = *view.world.get<GlobalPosition>(main_camera);
-        const auto *positions = view.column<GlobalPosition>();
-        const auto *sizes = view.column<Size>();
-        const auto *colors = view.column<Color>();
-        const auto *targets = view.optional<CameraTarget>();
+        const auto* positions = view.column<GlobalPosition>();
+        const auto* sizes = view.column<Size>();
+        const auto* colors = view.column<Color>();
+        const auto* targets = view.optional<CameraTarget>();
 
         for (uint i = 0; i < view.count(); i++) {
             const GlobalPosition camera_position = targets == nullptr
@@ -41,29 +41,35 @@ SYSTEM(RenderRectSys, With<GlobalPosition, Size, Color>, On<Render>) {
     }
 };
 
-SYSTEM(RenderSpriteSys, With<GlobalPosition, Sprite>, On<Render>) {
+SYSTEM(RenderSpriteSys, With<GlobalPosition, Sprite, Size>, On<Render>) {
     ITER(view) {
         const ecs::Entity main_camera = camera_plugin_impl::mainCamera(view.world);
         const GlobalPosition main_camera_position = *view.world.get<GlobalPosition>(main_camera);
-        const auto *positions = view.column<GlobalPosition>();
-        const auto *sprites = view.column<Sprite>();
-        const auto *targets = view.optional<CameraTarget>();
+        const auto* positions = view.column<GlobalPosition>();
+        const auto* sprites = view.column<Sprite>();
+        const auto* targets = view.optional<CameraTarget>();
+        const auto* sizes = view.column<Size>();
 
         for (uint i = 0; i < view.count(); i++) {
             const GlobalPosition camera_position = targets == nullptr
                                                        ? main_camera_position
                                                        : *view.world.get<GlobalPosition>(targets[i].camera);
+
+
+            const float diffx = std::abs(sizes[i].width - static_cast<float>(sprites[i].rect.width));
+            const float diffy = std::abs(sizes[i].height - static_cast<float>(sprites[i].rect.height));
             view.world.api->drawSprite(
-                render_plugin_impl::worldToCamera(positions[i], camera_position),
+                render_plugin_impl::worldToCamera({
+                                                      positions[i].x + diffx / 2.f, positions[i].y + diffy / 2.f
+                                                  }, camera_position),
                 sprites[i]
             );
         }
     }
 };
 
-
 struct RenderPlugin {
-    void load(ecs::World &world) {
+    void load(ecs::World& world) {
         world.plugin<CameraPlugin>();
         world.registerComponent<Size>();
         world.registerComponent<Color>();
@@ -71,7 +77,7 @@ struct RenderPlugin {
         world.system<RenderSpriteSys>();
     }
 
-    void unload(ecs::World &world) {
+    void unload(ecs::World& world) {
         world.remove<RenderRectSys>();
         world.remove<RenderSpriteSys>();
     }
