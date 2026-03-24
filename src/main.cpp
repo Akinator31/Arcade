e43d0023febe643fada233ffa4f47f5978ad371e
Akinator31 <paveldewavrechin@gmail.com>
2026-03-24 19:06:40 +0100

    refactor(main): add unique_ptr for lib loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include "core/dynamic/GameLoader.hpp"
 #include "core/dynamic/GraphicsLoader.hpp"
 #include "IDisplayModule.hpp"
@@ -9,15 +10,17 @@ int main(const int argc, char** argv) {
         return 1;
     }
 
-    GraphicsApiLoader graphics_loader(argv[1]);
-    GameLoader game_loader("cmake-build-debug/lib/arcade_game_menu.so");
+    auto graphics_loader = std::make_unique<GraphicsApiLoader>(argv[1]);
+    auto game_loader = std::make_unique<GameLoader>("cmake-build-debug/lib/arcade_game_menu.so");
 
-    IDisplayModule* api = graphics_loader.call<Create>();
-    IGameModule* game = game_loader.call<LoadGame>();
+    IDisplayModule* api = graphics_loader->call<Create>();
+    IGameModule* game = game_loader->call<LoadGame>();
 
     api->init();
     api->loadResources(game->getResources());
-    while (api->isWindowOpen()) {
+    bool shouldQuit = false;
+
+    while (api->isWindowOpen() && !shouldQuit) {
         api->beginFrame();
 
         game->update(api);
@@ -25,18 +28,24 @@ int main(const int argc, char** argv) {
         if (auto action = game->consumeCoreAction(); action.has_value()) {
             switch (action->type) {
             case CoreActionType::SwitchGame: {
-                game_loader.call<UnloadGame>(game);
-                game = GameLoader(action->target).call<LoadGame>();
+                game_loader->call<UnloadGame>(game);
+                game_loader = std::make_unique<GameLoader>(action->target);
+                game = game_loader->call<LoadGame>();
+                api->loadResources(game->getResources());
                 break;
             }
             case CoreActionType::SwitchGraphics: {
-                graphics_loader.call<Destroy>(api);
-                api = GraphicsApiLoader(action->target).call<Create>();
+                api->shutdown();
+                graphics_loader->call<Destroy>(api);
+                graphics_loader = std::make_unique<GraphicsApiLoader>(action->target);
+                api = graphics_loader->call<Create>();
+                api->init();
                 api->loadResources(game->getResources());
                 break;
             }
 
             case CoreActionType::Quit: {
+                shouldQuit = true;
                 break;
             }
 
@@ -49,7 +58,7 @@ int main(const int argc, char** argv) {
     }
     api->shutdown();
 
-    game_loader.call<UnloadGame>(game);
-    graphics_loader.call<Destroy>(api);
+    game_loader->call<UnloadGame>(game);
+    graphics_loader->call<Destroy>(api);
     return 0;
 }
